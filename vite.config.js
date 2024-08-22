72949299eed6fc45a8d0e388e3135da46e8d95d9
dclisbee <dmclisbee@gmail.com>
2024-08-22 18:25:21 -0500

    added gh pages functionality hopefully

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/portfolio-react-threejs",
+  plugins: [
+    react(),
+    ghPages(),
+  ],
 })
